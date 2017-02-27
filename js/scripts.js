e3fc9ceb1e5af13aa84de4501637933331f0086d
Brian Marete <bmarete10@gmail.com>
2017-02-27 15:14:14 +0300

    Update UI logic for show-contact section

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,30 @@
 // Business Logic
 function Contact(first, last) {
+  'use strict';
   this.firstName = first;
   this.lastName = last;
 }
 
 // User Interface Logic
-$(document).ready(function() {
-  $("form#new-contact").submit(function(event) {
+$(document).ready(function () {
+  'use strict';
+  $("form#new-contact").submit(function (event) {
     event.preventDefault();
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var inputtedFirstName = $("input#new-first-name").val(),
+      inputtedLastName = $("input#new-last-name").val(),
+      newContact = new Contact(inputtedFirstName, inputtedLastName);
 
     $("ul#contacts").append('<li><span class="contact">' + newContact.firstName + "</span></li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+
+    $(".contact").last().click(function () {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
   });
 });
