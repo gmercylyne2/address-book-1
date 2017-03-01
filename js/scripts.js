67906792fb1fc82f9f39fc66938c1cd56f90caae
mercy <gmercylyne@gmail.com>
2017-03-01 14:51:05 +0300

    add address constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,42 @@
 // Business Logic
 function Contact(first, last) {
-  'use strict';
-  this.firstName = first;
-  this.lastName = last;
-  this.addresses =[];
+    'use strict';
+    this.firstName = first;
+    this.lastName = last;
+    this.addresses = [];
 }
 
-Contact.prototype.fullName = function () {
-  return this.firstName + " " + this.lastName;
+Contact.prototype.fullName = function() {
+    return this.firstName + " " + this.lastName;
+}
+
+function Address(street, city, county) {
+    this.street = street;
+    this.city = city;
+    this.county = county;
 }
 
 // User Interface Logic
-$(document).ready(function () {
-  'use strict';s
-  $("form#new-contact").submit(function (event) {
-    event.preventDefault();
+$(document).ready(function() {
+    'use strict';
+    s
+    $("form#new-contact").submit(function(event) {
+        event.preventDefault();
 
-    var inputtedFirstName = $("input#new-first-name").val(),
-      inputtedLastName = $("input#new-last-name").val(),
-      newContact = new Contact(inputtedFirstName, inputtedLastName);
+        var inputtedFirstName = $("input#new-first-name").val(),
+            inputtedLastName = $("input#new-last-name").val(),
+            newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append('<li><span class="contact">' + newContact.fullName() + "</span></li>");
+        $("ul#contacts").append('<li><span class="contact">' + newContact.fullName() + "</span></li>");
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
+        $("input#new-first-name").val("");
+        $("input#new-last-name").val("");
 
-    $(".contact").last().click(function () {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.firstName);
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
+        $(".contact").last().click(function() {
+            $("#show-contact").show();
+            $("#show-contact h2").text(newContact.firstName);
+            $(".first-name").text(newContact.firstName);
+            $(".last-name").text(newContact.lastName);
+        });
     });
-  });
 });
